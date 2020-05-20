be42d257f1f568c803c89efb4317cfbf99c3760d
Eder <edersonschaukoski@gmail.com>
2020-05-20 11:36:13 -0300

    Adicionado codeigniter

---
+++ index_old.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'rb-postgres.php';
+/*
+R::setup('pgsql:host=ec2-35-171-31-33.compute-1.amazonaws.com;dbname=dc98cfascbki6p', 'mxhxugrovwdpya', '06f3b4d30b3e19f4dbdf3cbf0b9bd52d6812d0438fed64216d46d8618c5854e0');
+
+$post = R::dispense('post');
+$post->title = 'My holiday';
+$id = R::store($post);
+*/
+echo "Hello World!";
+?>
